9eac5188ae64a1b369aec785b55c0c0ae940ab16
Rafaelgarra <rafaelgaara1@gmail.com>
2026-02-05 16:10:51 -0300

    cors adjust

--- app/main.py
+++ app/main.py
@@ -13,6 +13,7 @@ app = FastAPI(title="GoldRush API - Neon Edition")
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://goldrush-web.vercel.app"
 ]
 
 app.add_middleware(
